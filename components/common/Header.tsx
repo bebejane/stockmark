10b777bf9d3854ddcdd5249719f9b08f764f7665
Bebe Jane <bjornthief@gmail.com>
2025-03-21 09:11:16 +0100

    fade header

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -5,24 +5,26 @@ import cn from 'classnames';
 import { render, renderNodeRule } from 'datocms-structured-text-to-html-string';
 import { isHeading, isInlineBlock } from 'datocms-structured-text-utils';
 import RevealHeader from '@components/common/RevealHeader';
+import { motion, MotionStyle } from 'framer-motion';
 
 export type HeaderProps = {
 	content: any;
 	video?: FileField;
 	margins?: boolean;
+	style?: MotionStyle;
 };
 
-export default function Header({ content, margins }: HeaderProps) {
+export default function Header({ content, margins, style }: HeaderProps) {
 	const headers = extractHeaders(content);
 
 	return (
-		<header className={cn(s.header, margins && s.margins)}>
+		<motion.header className={cn(s.header, margins && s.margins)} style={style}>
 			{headers.map(({ text, className }, i) => (
 				<RevealHeader key={i} size={1} className={className} delay={0.2 * i}>
 					{text}
 				</RevealHeader>
 			))}
-		</header>
+		</motion.header>
 	);
 }
 
@@ -35,8 +37,7 @@ export function extractHeaders(content: any): { text: string; className: string 
 				return renderNode(`h${node.level + 1}`, { key, className: 'right' }, children);
 			}),
 			renderNodeRule(isInlineBlock, ({ adapter: { renderNode }, node, children, key }) => {
-				//headers.push({ text: children, className: node.style });
-
+				// Replace with video thumbnail
 				return '###';
 			}),
 		],
