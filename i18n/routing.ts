85a707f2f57234c2efc88533ce61aa92e390ab46
Bebe Jane <bjornthief@gmail.com>
2025-03-25 10:52:50 +0100

    pathname routing

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,11 +1,34 @@
-
 import { createNavigation } from 'next-intl/navigation';
+
 import { defineRouting } from 'next-intl/routing';
 
 export const locales = ['sv', 'en'];
 export const defaultLocale = 'sv';
 export const localePrefix = 'as-needed';
-export const routing = defineRouting({ locales, localePrefix, defaultLocale });
+export const routing = defineRouting({
+  locales,
+  localePrefix,
+  defaultLocale,
+  pathnames: {
+    '/': '/',
+    '/om-oss': {
+      sv: '/om-oss',
+      en: '/about',
+    },
+    '/om-dig': {
+      sv: '/om-dig',
+      en: '/you',
+    },
+    '/manifest': {
+      sv: '/om-oss',
+      en: '/about',
+    },
+    '/portfolj': {
+      sv: '/portfolj',
+      en: '/portfolio',
+    }
+  }
+});
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);
