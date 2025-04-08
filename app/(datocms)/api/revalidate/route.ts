7591013b9930534e9bc0406d431db27bb816cd35
Bebe Jane <bjornthief@gmail.com>
2025-04-08 14:24:55 +0200

    test

--- app/(datocms)/api/revalidate/route.ts
+++ app/(datocms)/api/revalidate/route.ts
@@ -15,6 +15,7 @@ export async function POST(req: Request) {
     const paths: string[] = []
     const path = await buildRoute(api_key, attributes, defaultLocale)
     locales.forEach(l => paths.push(`/${l}${path}`))
+    paths.push('/')
     const tags: string[] = [api_key, id].filter(t => t)
     return await revalidate(paths, tags, true)
   })
